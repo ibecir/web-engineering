21be6d3f2d7c1558bc774cef9aa20cb3370e42d2
ibecir <becir@bioptimizers.com>
2023-10-03 21:23:41 +0200

    Remove filters

--- src/main/java/ba/edu/ibu/webengineering/core/api/auth/AuthorizationApi.java
+++
@@ -1,9 +0,0 @@
-package ba.edu.ibu.webengineering.core.api.auth;
-
-import ba.edu.ibu.webengineering.core.model.User;
-
-public interface AuthorizationApi {
-    String generateToken(User user);
-
-    User decodeUser(String token);
-}
--- src/main/java/ba/edu/ibu/webengineering/rest/annotations/PermissionGroup.java
+++
@@ -1,5 +0,0 @@
-package ba.edu.ibu.webengineering.rest.annotations;
-
-public enum PermissionGroup {
-    ADMINISTRATOR, WRITER, READER, PUBLIC
-}
